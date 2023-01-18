ad7dfab5ea32225ed24795ea56bba4adf3aa03f8
Brandon Cha <brandon.cha@redspace.com>
2023-01-18 15:14:22 -0500

    --section 5

---
+++ cmp-databinding/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,39 @@
+import {
+  Component, ElementRef, EventEmitter, OnInit, Output, ViewChild
+} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output('svrCreated') serverCreated = new EventEmitter<{
+    serverName: string, serverContent: string
+  }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{
+    blueprintName: string, blueprintContent: string
+  }>();
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    })
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      blueprintName: nameInput.value,
+      blueprintContent: this.serverContentInput.nativeElement.value
+    });
+  }
+}
