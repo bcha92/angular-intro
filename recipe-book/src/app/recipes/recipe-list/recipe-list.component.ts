5ec6e324944984df8998571506ab7acfa654e3a5
Brandon Cha <brandon.cha@redspace.com>
2023-01-20 15:28:21 -0500

    --section 6

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import Recipe from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import Recipe from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
@@ -19,4 +21,8 @@ export class RecipeListComponent {
       'https://lobsterfrommaine.com/wp-content/uploads/2022/05/20210503-MLMC-Spring-Radish-Pea-Lobster-Salad-Labneh2883-scaled.jpg'
     )
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -9,6 +9,10 @@ import Ingredient from '../shared/ingredient.model';
 export class ShoppingListComponent {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
+    new Ingredient('Tomatoes', 10),
   ];
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
